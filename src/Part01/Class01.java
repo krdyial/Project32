43659a2ddcd397ac8aaa44f45cf275bd5d3a5a0f
Alaettin <alaettinkaradayi@gmail.com>
2020-11-04 01:04:55 +0100

    second step

--- src/Part01/Class01.java
+++ src/Part01/Class01.java
@@ -6,9 +6,18 @@ public class Class01 {
 
         Class02 obj = new Class02();
         obj.multiOperation(3,2); //9
-        System.out.println(obj.addition(104, 5));
+        System.out.println(obj.addition(104, 5)); //109
+
+        System.out.println(obj.substruction(5, 10));//  -5
 
 
+        obj.multiOperation(2,3); //7
+
+        int a = 10;
+
+        for (int i= 0; i<a; i++){
+            System.out.print(a*i+ " ");
+        }
 
     }
 
